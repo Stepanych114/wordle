f96f05031c201bb63c7ccbfb6510b55d129be183
Stepanych114 <danikstepanyan.2015@mail.ru>
2024-05-26 01:02:47 +0300

    add new version

--- lib/wordle_my_game.rb
+++ lib/wordle_my_game.rb
@@ -1,36 +1,35 @@
 # frozen_string_literal: true
-require_relative 'wordle/word_list'
 class Wordle
   def initialize(word)
-    @word =word.upcase
+    @word = word.upcase
     @attempts = 6
-    "_"*word.length
+    "_" * word.length
   end
   def try(attempt)
-    alrwas=""
+    alrwas = ""
     if attempt.length != @word.length
       return "Неверная длина слова. Попробуйте еще раз."
     end
-    s=attempt.upcase
-    result=""
-    if s==@word
+    s = attempt.upcase
+    result = ""
+    if s == @word
       return "Ответ найден, Вы победили!"
     end
-    @attempts-=1
+    @attempts -= 1
     @word.chars.each_with_index do |c, i|
-      if c == s[i] && alrwas.count(s[i].upcase)<@word.count(s[i].upcase)
-        result+="🟩"
-        alrwas+=s[i].upcase
-      elsif @word.include?(s[i]) && alrwas.count(s[i].upcase)<@word.count(s[i].upcase)
-        result+="🟨"
-        alrwas+=s[i].upcase
+      if c == s[i] && alrwas.count(s[i]) < @word.count(s[i])
+        result += "🟩"
+        alrwas += s[i]
+      elsif @word.include?(s[i]) && alrwas.count(s[i].upcase) < @word.count(s[i])
+        result += "🟨"
+        alrwas + s[i]
       else
-        result+="⬛"
+        result += "⬛"
       end
     end
-    if result=="🟩"*@word.length
-      return "Ответ найден, ты победил!"
-    elsif @attempts<=0
+    if result == "🟩"*@word.length
+      return "Ответ найден, Вы победили!"
+    elsif @attempts == 0
       return "Попыток не осталось. Ответом было слово: '#{@word}'. Вы проиграли!"
     end
     return "Попыток осталось: #{@attempts} | Результат: #{result}"
--- wordle_my_game.gemspec
+++ wordle_my_game.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'wordle_my_game'
-  s.version = '0.1.2'
+  s.version = '0.1.4'
   s.authors = ['Daniil Stepanyan']
   s.summary = "Логика для игры типа Wordle с произвольным числом букв в слове."
   s.files = Dir['lib/**/*.rb']
